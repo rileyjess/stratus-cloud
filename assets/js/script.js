1bb028f13df7fe3edca4a19f6e21b16a8eed258f
good morning <172862789+jr-jpg@users.noreply.github.com>
2024-08-06 15:43:36 -0500

    Psuedocoded the JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Call data from the API
+
+// Create a card to display current conditions
+// with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+
+// Create a card to display 5-day forecast
+// featuring the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// Append cards to the dashboard
+
+// Search button click event
+
+// Store search history in local storage
+
+// Display search results on the page
+
+// When a person clicks a search history listing, the data for that city displays again
