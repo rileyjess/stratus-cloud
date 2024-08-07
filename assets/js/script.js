249a02e30e1e33f3d26894247f98caf7790b4714
good morning <172862789+jr-jpg@users.noreply.github.com>
2024-08-07 12:37:37 -0500

    Created variables and functions in the JavaScript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
+// Assign the API Key to a variable
+const APIKey = "df9c79446d45e8f51367cc15f887cb31";
+const fetchBtn = document.getElementById('submit-btn');
+searchHistoryEl = document.getElementById('search-history');
+currentConditionsEl = document.getElementById('current-conditions');
+forecastEl = document.getElementById('forecast');
+
 // Call data from the API
 
 // Create a card to display current conditions
 // with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+function currentConditions(city) {
+    const requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (currentConditionsData) {
+        console.log(currentConditionsData);
+    })
+}
+
 
 // Create a card to display 5-day forecast
 // featuring the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+function futureForecast() {
+    const requestUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+}
 
 // Append cards to the dashboard
 
-// Search button click event
+// Submit event when search button is clicked
+fetchBtn.addEventListener('submit', currentConditions);
+fetchBtn.addEventListener('submit', futureForecast);
 
 // Store search history in local storage
 
