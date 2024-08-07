5331041c97e05c7a2a0836a119f7afc97a716957
good morning <172862789+jr-jpg@users.noreply.github.com>
2024-08-07 16:09:39 -0500

    Added logic to the forecast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,49 +21,85 @@ function currentConditions(city) {
         .then(function (currentConditionsData) {
             console.log(currentConditionsData);
 
-            for (let i = 0; i < currentConditionsData.length; i++) {
-                // Create elements for each of the data
-                const currentConditionsCard = document.createElement('div');
-                const cityNameAndDate = document.createElement('h2');
-                const currentTemperature = document.createElement('p');
-                const currentWindSpeed = document.createElement('p');
-                const currentHumidity = document.createElement('p');
+            // Create elements for each of the data points
+            const currentConditionsCard = document.createElement('div');
+            const cityNameAndDate = document.createElement('h2');
+            const currentTemperature = document.createElement('p');
+            const currentWindSpeed = document.createElement('p');
+            const currentHumidity = document.createElement('p');
 
-                // Find the current date
-                const date = new Date();
-                const day = date.getDate();
-                const month = date.getMonth() + 1;
-                const year = date.getFullYear();
-                let currentDate = `(${day}/${month}/${year})`;
+            // Find the current date
+            const date = new Date();
+            const day = date.getDate();
+            const month = date.getMonth() + 1;
+            const year = date.getFullYear();
+            let currentDate = `(${day}/${month}/${year})`;
 
-                // Set text content for each new element based on the data points
-                cityNameAndDate.textContent = currentConditionsData[i].name + " " + currentDate + " " + currentConditionsData[i].weather, icon;
-                currentTemperature.textContent = "Temp: " + currentConditionsData[i].main.temp;
-                currentWindSpeed.textContent = "Wind" + currentConditionsData[i].wind.speed;
-                currentHumidity.textContent = "Wind: " + currentConditionsData[i].main.humidity;
+            // Set text content for each new element based on the data points
+            cityNameAndDate.textContent = response.currentConditionsData[i].name + " " + currentDate + " " + response.currentConditionsData[i].weather.icon;
+            currentTemperature.textContent = "Temp: " + response.currentConditionsData[i].main.temp + " °F";
+            currentWindSpeed.textContent = "Wind: " + response.currentConditionsData[i].wind.speed + " MPH";
+            currentHumidity.textContent = "Humidity: " + response.currentConditionsData[i].main.humidity + " %";
 
-                // Append the new elements to the current conditions card
-                currentConditionsCard.append(cityNameAndDate);
-                currentConditionsCard.append(currentTemperature);
-                currentConditionsCard.append(currentWindSpeed);
-                currentConditionsCard.append(currentHumidity);
+            // Append the new elements to the current conditions card
+            currentConditionsCard.append(cityNameAndDate);
+            currentConditionsCard.append(currentTemperature);
+            currentConditionsCard.append(currentWindSpeed);
+            currentConditionsCard.append(currentHumidity);
 
-                // Append the card to the right container in the HTML
-                currentConditionsContainer.append(currentConditionsCard);
-            }
+            // Append the card to the right container in the HTML
+            currentConditionsContainer.append(currentConditionsCard);
         })
-
-
 }
+
 
 
 // Create a card to display 5-day forecast
 // featuring the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 function futureForecast() {
     const requestUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
-}
 
-// Append cards to the dashboard
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (forecastData) {
+            console.log(forecastData);
+
+            const dailyForecasts = document.querySelectorAll(".daily-forecast");
+            for (let i = 0; i < dailyForecasts.length; i++) {
+                // Create elements for each of the data points
+                const forecastDate = document.createElement('p');
+                const forecastIcon = document.createElement('p');
+                const forecastTemperature = document.createElement('p');
+                const forecastWindSpeed = document.createElement('p');
+                const forecastHumidity = document.createElement('p');
+
+                // Find the current date
+                const date = new Date();
+                const day = date.getDate();
+                const month = date.getMonth() + 1;
+                const year = date.getFullYear();
+                let currentDate = `${day}/${month}/${year}`;
+
+                // Set text content for each new element based on the data points
+                forecastDate.textContent = currentDate;
+                forecastIcon.textContent = response.forecastData[i].weather.icon
+                forecastTemperature.textContent = "Temp: " + response.forecastData[i].main.temp + " °F";
+                forecastWindSpeed.textContent = "Wind: " + response.forecastData[i].wind.speed + " MPH";
+                forecastHumidity.textContent = "Humidity: " + response.forecastData[i].main.humidity + " %";
+
+                // Append the new elements to the forecast divs
+                dailyForecasts.append(forecastDate);
+                dailyForecasts.append(forecastIcon);
+                dailyForecasts.append(forecastTemperature);
+                dailyForecasts.append(forecastWindSpeed);
+                dailyForecasts.append(forecastHumidity);
+            }
+        })
+
+}
 
 // Submit event when search button is clicked
 fetchBtn.addEventListener('submit', currentConditions);
